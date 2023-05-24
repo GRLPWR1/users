3187d68d011f76c1be8a3a3df312968b15f90c2c
GRLPWR1 <110125337+GRLPWR1@users.noreply.github.com>
2023-05-25 02:22:42 +0300

    markup done

--- users/src/App.js
+++ users/src/App.js
@@ -1,12 +1,15 @@
-
-import './App.css';
+import Header from './components/Header/Header.jsx';
+import Users from './components/Users/Users.jsx';
+import Footer from './components/Footer/Footer.jsx';
 import { v4 as uuidv4 } from 'uuid';
 uuidv4();
 
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Users />
+      <Footer />
     </div>
   );
 }
--- users/src/components/Header/Header.jsx
+++ users/src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-function Header(props) {
+function Header() {
     return (
-        <div>
-
-        </div>
+        <nav>
+            <div className="nav-wrapper">
+                <h1 className="brand-logo" style={{ marginLeft: 0.5 + 'em' }}>Random Users</h1>
+                <ul className="right hide-on-med-and-down">
+                    <li><a style={{ marginTop: 0.5 + 'em' }} className="waves-effect waves-light btn-large">Get 10 random users</a></li>
+                </ul>
+            </div>
+        </nav >
     );
 }
 
